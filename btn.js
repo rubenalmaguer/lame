06ab12aa6d635d6c0e90f0cc073c79900a509f8b
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2022-02-09 15:52:29 +0900

    Update btn.js

--- btn.js
+++ btn.js
@@ -20,7 +20,7 @@ rxProApplicant = /a3\.flit\.to\/#\/pro-tr\/pro-applicant\/\d/;
 /* FLOW */
 if (rxCrowdMonitoring.test(currentUrl)) {
   if (!document.head.querySelectorAll(`[src="${releaseRoot}crowd-monitoring.js"]`).length) { injectRemoteScript(`${releaseRoot}crowd-monitoring.js`) }
-  else { translateCK() }
+  else { location.reload }
 }
 
 else if (
