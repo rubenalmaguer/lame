063f2fa112f4469b8155076445d2b53cbed2238f
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2022-04-26 14:45:55 +0900

    2.0 (netlify as CDN)

--- btn.js
+++ btn.js
@@ -1,50 +1,35 @@
-releaseTag = 'v1.2.3';
+LAME_CDN = `https://lame.netlify.com/`;
+LAME_CURRENT_URL = window.location.href;
+LAME_SUPPORTED_SITES = [
+  {rx: /lame\.netlify\.app/, scriptSrc: `${LAME_CDN}project-page.js`},
+  {rx: /a\.flit\.to:4435\/admin#\/admin\/req_tr/, scriptSrc: `${LAME_CDN}crowd-monitoring.js`},
+  {rx: /a3\.flit\.to\/#\/arcade\/arcade-objection/, scriptSrc: `${LAME_CDN}arcade-objection.js`},
+  {rx: /a3\.flit\.to\/#\/arcade\/arcade-user-history/, scriptSrc: `${LAME_CDN}arcade-history.js`},
+  {rx: /api-demo\.flit\.to/, scriptSrc: `${LAME_CDN}extend-simi-sites.js`},
+  {rx: /translators\.to/, scriptSrc: `${LAME_CDN}extend-simi-sites.js`},
+  {rx: /a3\.flit\.to\/#\/pro-tr\/pro-applicant\/\d/, scriptSrc: `${LAME_CDN}pro-applicant.js`},
+];
 
-/* SETTINGS */
-/* UIlang = 'en'; */
-susLimit = 85;
-setting = (typeof setting == 'undefined' || setting == 'word') ? 'char' : 'word'; /* for objection similarity */
-
-/* LINKS */
-releaseRoot = `https://cdn.jsdelivr.net/gh/dokdospanish/lame@${releaseTag}/`;
-currentUrl = window.location.href;
-
-/* REGEX */
-rxLameSite = /lame\.netlify\.app/;
-rxCrowdMonitoring = /a\.flit\.to:4435\/admin#\/admin\/req_tr/;
-rxObjection = /a3\.flit\.to\/#\/arcade\/arcade-objection/;
-rxFlittoApi = /api-demo\.flit\.to/;
-rxTranslatorsTo = /translators\.to/;
-rxProApplicant = /a3\.flit\.to\/#\/pro-tr\/pro-applicant\/\d/;
-
-/* FLOW */
-if (rxCrowdMonitoring.test(currentUrl)) {
-  if (!document.head.querySelectorAll(`[src="${releaseRoot}crowd-monitoring.js"]`).length) { injectRemoteScript(`${releaseRoot}crowd-monitoring.js`) }
-  else { location.reload() }
+matchedScript = '';
+for (let {rx, scriptSrc} of LAME_SUPPORTED_SITES) {
+  if (rx.test(LAME_CURRENT_URL)) { matchedScript = scriptSrc }
 }
 
-else if (
-  rxObjection.test(currentUrl)
-  ) { injectRemoteScript(`${releaseRoot}objection-compare.js`) }
+if (matchedScript) {
+  if (!document.head.querySelectorAll(`[src="${matchedScript}"]`).length) {
+    injectRemoteScript(matchedScript).then(()=> { lameify() })
+  }
+  else {
+    lameify()
+  }
+}
 
-else if (
-  rxFlittoApi.test(currentUrl)
-  || rxTranslatorsTo.test(currentUrl)
-  ) { injectRemoteScript(`${releaseRoot}mt-similarity.js`) }
-
-else if (  rxLameSite.test(currentUrl)
-  ) { alert('1. Drag button to bookmarks bar.\n\n2. Use button in one of these sites:\n   - Vitamin\'s Arcade Objection\n   - Flitto\'s API demo\n   - translators.to \n   - Old Flitto Admin > crowd_tr > req_tr') }
-
-
-
-
-/* HELPERS */
 function injectRemoteScript(src) {
   return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src = src;
-      script.addEventListener('load', resolve);
-      script.addEventListener('error', e => reject(e.error));
-      document.head.appendChild(script);
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => resolve(/* script */);
+    script.onerror = () => reject(new Error(`Script load error for ${src}`));
+    document.head.append(script);
   });
 }
