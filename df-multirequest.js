71810535078fdba8c8b93f9fb7a42bf2af9920c0
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2022-04-26 17:04:07 +0900

    Create df-multirequest.js

---
+++ df-multirequest.js
@@ -0,0 +1,44 @@
+function lameify() {
+  /* SAME-LANG: .svg-translate, DF: .svg-translate.purple, LITE: .svg-translate.blue AI: PURPLE: .svg-translate.yellow*/
+  let mainPostButtonsYellowCount = [...document.querySelectorAll('.df-post__content .svg-translate.yellow')].length;
+  if (mainPostButtonsYellowCount < 1) { return }
+
+  let path = window.location.pathname.split('/');
+  let userLang = (path[1] == 'post') ? 'en': path[1];
+  let postId = path[path.length - 1];
+  if (confirm(`Do you want to make ${mainPostButtonsYellowCount} ${(mainPostButtonsYellowCount > 1) ? 'requests' : 'request'}?\nLanguage code: ${userLang}`)) {
+
+    let mainPostButtonsAll = [...document.querySelectorAll('.df-post__content .df-btn--translate')]; 
+    mainPostButtonsAll.forEach( (b, i) => {
+      if (b.querySelector('.yellow')) {
+
+        requestLiteTranslation(postId, userLang, i)
+      }
+    });
+    
+  }
+}
+
+
+function requestLiteTranslation(postId, targetLang, index) {
+  let payload = (index == 0) ? `{"type":"crowd","post_id":"${postId}","lang":"${targetLang}","dest":{"type":"post","title":true}}`
+  : `{"type":"crowd","post_id":"${postId}","lang":"${targetLang}","dest":{"type":"post","title":false,"content_index":${(index).toString()}}}`;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      authority: 'www.desertfox.io:5501',
+      authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjIzYWUxYmE0YTliMzc5M2MwMzczNzExIiwiaWF0IjoxNjQ5MTM4MjQwLCJleHAiOjE2ODA2NzQyNDB9.S2XCdTaY5n3uX12NQ1umycA0kQvApqmiSY4uqGQKYi8',
+      'content-type': 'application/json'
+    },
+    body: payload
+  };
+
+  console.log(options);
+
+  fetch('https://www.desertfox.io:5501/post/flitto-translate', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+}
