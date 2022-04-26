72744511e28c5b5de015f37bb068f77599b04d7c
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2022-04-26 14:51:08 +0900

    Create project-page.js

---
+++ project-page.js
@@ -0,0 +1,3 @@
+function lameify() {
+  alert('1. Drag button to bookmarks bar.\n\n2. Use button in one of these sites:\n   - Vitamin\'s Arcade Objection\n   - Flitto\'s API demo\n   - translators.to \n   - Old Flitto Admin > crowd_tr > req_tr')
+}
