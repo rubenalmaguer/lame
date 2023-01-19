68b9542e6e06fb3c079152cdaaa8059b997ef699
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2023-01-19 17:21:44 +0900

    Update flitto.js

--- flitto.js
+++ flitto.js
@@ -1,3 +1,29 @@
+LAME_OLD_HREF = window.location.href;
+
+new MutationObserver(mutations => {
+    mutations.forEach(() => {
+      if (LAME_OLD_HREF != winddow.location.href) {
+        LAME_OLD_HREF = window.location.href;
+        lameify();
+      }
+    });
+  })
+.observe(document.querySelector('body'), { childList: true, subtree: true });
+
+
+/*
+LAME_ROUTES = [
+  // New Arcade
+  {rx: /api-demo\.flit\.to/, funcName: `${LAME_CDN}extend-simi-sites.js`},
+  {rx: /translators\.to/, funcName: `${LAME_CDN}extend-simi-sites.js`},
+  {rx: /desertfox\.io.+post/, funcName: `${LAME_CDN}df-multirequest.js`},
+  
+];
+window['lameify']()
+*/
+
+
+
 function lameify() {
   alert('Pulito!')
 }
