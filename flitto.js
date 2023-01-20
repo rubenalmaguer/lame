dfdc3671d99d18ecfb90c0cd20886c8bd3eedf3f
dokdospanish <67682780+dokdospanish@users.noreply.github.com>
2023-01-20 10:01:35 +0900

    Update flitto.js

--- flitto.js
+++ flitto.js
@@ -1,52 +1,54 @@
-LAME_OLD_HREF = '';
-
-
-function checkLocation() {
-  const myLoc = window.location.href;
-  if (!myLoc.includes('arcade-service')) return
-  
-  if (LAME_OLD_HREF == myLoc
-      || myLoc === 'https://www.flitto.com/arcade-service/problems/32'
-      || myLoc === 'https://www.flitto.com/arcade-service/problems/193') {
-    document.querySelector('i.checkbox__icon')?.click();
-    document.querySelector('.text-button.primary-light-fill.start-btn')?.click();
-    return
-  }
-    
-  if (/problems\/\w+\/\w+$/.test(myLoc)) {
-      //example:
-      //https://www.flitto.com/arcade-service/problems/63c539909f134b08daa48d93/63c539909f134b08daa48d87
-    
-      LAME_OLD_HREF = myLoc;
-      
-      // Wait for load
-      setTimeout(()=>{
-      // Make target div contenteditable, so the spellcheck extension kicks in
-          
-      let targetArea = document.querySelector('p.translation__content');
-      targetArea?.setAttribute('contenteditable','true');
-      targetArea?.focus()
-      
-      // Final accept
-      let acceptBtn = document.querySelector('.accept-button');
-      acceptBtn?.addEventListener('click', ()=>{
-        setTimeout(()=>{
-          sendBtn = document.querySelector('button.primary-light-fill');
-          sendBtn?.click()
-          }, 50);
-      });
-          
-      // For re-reviewing. Remove irrelevant, already accepted translation.
-      document.querySelector('.translation__re-accepted-text').closest('.translation').remove();
-          
-    }, 500);
-      
-  }
-    
-}
-
-
+LAME_OLD_LOCATION = '';
+TERMS_CONDITIONS_URLS = [
+  'https://www.flitto.com/arcade-service/problems/32',
+  'https://www.flitto.com/arcade-service/problems/193'
+];
 
 function lameify() {
   setInterval(checkLocation, 100);
 }
+
+function checkLocation() {
+  const myLocation = window.location.href;
+
+  // Exit if not first time or not in New Arcade
+  if (myLocation == LAME_OLD_LOCATION || !myLocation.includes('arcade-service')) return;
+
+  //
+  if (TERMS_CONDITIONS_URLS.includes(myLocation)) {
+    bypassTermsAndConditions();
+    return;
+  }
+  
+  // It's a "problem/question" page like https://www.flitto.com/arcade-service/problems/63c539909f134b08daa48d93/63c539909f134b08daa48d87
+  if (/problems\/\w+\/\w+$/.test(myLocation)) {
+    LAME_OLD_LOCATION = myLoc;
+    setTimeout(tweakProblemPage, 500);
+  } 
+}
+
+function bypassTermsAndConditions() {
+  document.querySelector('i.checkbox__icon')?.click();
+  document.querySelector('.text-button.primary-light-fill.start-btn')?.click();
+}
+
+function tweakProblemPage() {
+  // Make target div contenteditable, so the spellcheck extension kicks in    
+  let targetAreas = [...document.querySelectorAll('p.translation__content')];
+  targetAreas?.forEach(area => area.setAttribute('contenteditable','true'));
+  targetAreas?.[0].focus();
+
+  // Auto-send afer Accept button is clicked
+  let acceptButtons = [...document.querySelectorAll('.accept-button')];
+  acceptButtons?.forEach(btn => btn.addEventListener('click', clickSend));
+      
+  // In case it's a re-review. Remove irrelevant, already accepted translation.
+  document.querySelector('.translation__re-accepted-text')?.closest('.translation')?.remove();
+}
+
+function clickSend() {
+  setTimeout(()=>{
+    document.querySelector('button.primary-light-fill')?.click()
+  }, 50);
+}
+
